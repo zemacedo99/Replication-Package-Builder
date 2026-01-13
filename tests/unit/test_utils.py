6b592da952f312777fe1ff2b646c2a56323dac91
Fernando França (フランサ) <github@furansa.me>
2026-01-13 09:32:33 +0000

    Chore: Add basic GitHub actions.

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -8,6 +8,6 @@ from src import utils  # pylint: disable=E0401
 class TestWritePrettyJSONToFileSuite:
     """Test suite for the write_pretty_json_to_file function."""
     def test_write_pretty_json_to_file_raises_validation_error_without_arguments(self) -> None:  # noqa: E501 pylint: disable=C0301
-        """Test if running without arguments fails."""
+        """Test if running without arguments raises validation error."""
         with pytest.raises(ValidationError):
             utils.write_pretty_json_to_file()
